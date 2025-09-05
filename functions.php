d485eeffaa15771a6603151d60151b8e5f206ed1
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-05 14:24:17 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1346,7 +1346,7 @@ function get__user_company_flag($author_id)
 
     $country = get_field('country', $company_id);
     $country_code = get_country_code_by_name($country);
-    return '<div class="flag">' . $company_id . ' </div>';
+    return '<div class="flag">' . $country . ' </div>';
 }
 function get__company_posts()
 {
