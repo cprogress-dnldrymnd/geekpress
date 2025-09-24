c9cda1d59badbf6dfdf9cee17435f6904e69d7e7
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-24 18:40:08 +0800

    Update page-acfform.php

--- page-acfform.php
+++ page-acfform.php
@@ -1,23 +1,28 @@
 <?php acf_form_head(); ?>
 <?php get_header(); ?>
 
-    <div id="primary">
-        <div id="content" role="main">
+<div id="primary">
+    <div id="content" role="main">
 
-            <?php /* The loop */ ?>
-            <?php while ( have_posts() ) : the_post(); ?>
-                
-                <h1><?php the_title(); ?></h1>
-                
-                <?php the_content(); ?>
-                
-                <p>My custom field: <?php the_field('my_custom_field'); ?></p>
-                
-                <?php acf_form(); ?>
+        <?php /* The loop */ ?>
+        <?php while (have_posts()) : the_post(); ?>
 
-            <?php endwhile; ?>
+            <h1><?php the_title(); ?></h1>
 
-        </div><!-- #content -->
-    </div><!-- #primary -->
+            <?php the_content(); ?>
+
+            <p>My custom field: <?php the_field('my_custom_field'); ?></p>
+
+            <?php acf_form(array(
+                'post_id'       => 1038,
+                'post_title'    => false,
+                'post_content'  => false,
+                'submit_value'  => __('Update meta')
+            )); ?>
+
+        <?php endwhile; ?>
+
+    </div><!-- #content -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
