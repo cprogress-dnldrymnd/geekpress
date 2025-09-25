03e486550ec4ffe7a20257bc2eef86ca50a53e63
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-25 16:35:41 +0800

    Update page-edit-company.php

--- page-edit-company.php
+++ page-edit-company.php
@@ -14,7 +14,7 @@ get_header();
 $current_user = wp_get_current_user();
 $user_id = $current_user->ID;
 
-$company_id = get__user_company($user_id, true);
+$company_id = get__user_company($user_id, false, true);
 
 $errors = [];
 $success = '';
