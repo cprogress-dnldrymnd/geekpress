18c6f4c7795f2f9f1e68d56b20257f586e75fcfc
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-26 18:00:29 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -2273,7 +2273,7 @@ function add_featured_image_to_rss_feed($content)
 
 
     $h2 = '<h2 style="margin-bottom: 15px;">';
-    $h2 .= '<a href="'.get_the_permalink($post->ID).'">';
+    $h2 .= '<a href="' . get_the_permalink($post->ID) . '">';
     $h2 .= get_the_title($post->ID);
     $h2 .= '</a>';
     $h2 .= '</h2>';
@@ -2282,15 +2282,17 @@ function add_featured_image_to_rss_feed($content)
         // Get the featured image HTML.
         // You can change 'medium' to 'thumbnail', 'large', etc.
         $featured_image = '<div style="margin-bottom: 15px;">';
-        $featured_image .= '<a href="'.get_the_permalink($post->ID).'">';
+        $featured_image .= '<a href="' . get_the_permalink($post->ID) . '">';
         $featured_image .= get_the_post_thumbnail($post->ID, 'large');
         $featured_image .= '</a>';
         $featured_image .= '</div>';
 
         // Prepend the image to the content (which is the excerpt in this case).
-        $content = $featured_image . $content;
+        $content = $h2 . $featured_image . $content;
+    } else {
+        $content = $h2 . $content;
     }
-    return $h2. $content;
+    return $content;
 }
 
 // Add the function to the RSS EXCERPT filter.
