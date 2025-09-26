3b9879b0717d43e1eb51379a4b9bd64eccf75ed1
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-26 17:48:46 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -2267,7 +2267,7 @@ function get__svg($name)
 
 /*end of post listing*/
 
-function add_featured_image_to_rss_feed($content) {
+function add_featured_image_to_rss_feed($title) {
     global $post;
     if ( has_post_thumbnail($post->ID) ) {
         // Get the featured image HTML.
@@ -2277,10 +2277,10 @@ function add_featured_image_to_rss_feed($content) {
         $featured_image .= '</div>';
 
         // Prepend the image to the content (which is the excerpt in this case).
-        $content = $featured_image . $content;
+        $content = $featured_image . $title;
     }
-    return $content;
+    return $title;
 }
 
 // Add the function to the RSS EXCERPT filter.
-add_filter('the_excerpt_rss', 'add_featured_image_to_rss_feed');
+add_filter('the_title_rss', 'add_featured_image_to_rss_feed');
