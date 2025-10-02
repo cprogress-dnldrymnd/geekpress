8e87e685bb51763988ccd9ef9b51294a4ff03fc8
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-02 18:06:50 +0800

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -152,7 +152,12 @@
 		<div class="homepage__wrapper">
 			<aside class="filter">
 				<form id="filter-form">
-					<h2 class="block__header block__header--filter">Filter By</h2>
+					<h2 class="block__header block__header--filter">
+						Filter By
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+							<path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
+						</svg>
+					</h2>
 					<div class="filter__block active">
 						<div class="filter__header  collapsible-trigger">
 							<h4>Categories</h4>
