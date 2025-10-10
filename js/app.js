de08b047df37a963d7764de41670fe024b62cc6d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-10 19:45:12 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,11 +7,7 @@ function filter() {
     $height = jQuery('.filter-block-inner').outerHeight();
     jQuery('.filter-block-inner').parent().css('--max-height', $height + 'px');
     jQuery('.block__header--filter').click(function (e) {
-      jQuery('.filter-block-parent').removeClass('remove-max-height');
       jQuery(this).toggleClass('active');
-      setTimeout(function () {
-        jQuery('.filter-block-parent').addClass('remove-max-height');
-      }, 300);
       e.preventDefault();
     });
   }
