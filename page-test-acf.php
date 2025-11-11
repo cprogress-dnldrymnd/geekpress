663037c2cabc45e1ad9bcf68b4bc2fb585bbba8b
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-11-11 23:22:50 +0800

    Create page-test-acf.php

---
+++ page-test-acf.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: ACF Test
+ */
+acf_form_head();
+get_header();
+?>
+
+<section class="page-reset-password">
+    <div class="container">
+       <?php acf_form(); ?>
+    </div>
+</section>
+
+<?php get_footer(); ?>
