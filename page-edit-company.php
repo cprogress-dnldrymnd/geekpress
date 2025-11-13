9e1144c41beb9fdc4fe1abe2f24a6088911b7139
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-11-13 19:29:22 +0800

    Update page-edit-company.php

--- page-edit-company.php
+++ page-edit-company.php
@@ -331,7 +331,12 @@ if (!is_company_manager(get_current_user_id(), $company_id)  && $company_id == 0
                         <div class="input__wrapper">
                             <label for="linkedin">LinkedIn</label>
                             <div class="input-group">
-                                <span class="prefix">@</span>
+                                <span class="prefix">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+  <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+  <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+</svg>
+                                </span>
                                 <input
                                     type="text"
                                     name="linkedin"
@@ -349,7 +354,12 @@ if (!is_company_manager(get_current_user_id(), $company_id)  && $company_id == 0
                         <div class="input__wrapper">
                             <label for="x">X (Twitter)</label>
                             <div class="input-group">
-                                <span class="prefix">@</span>
+                                <span class="prefix">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+  <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+  <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+</svg>
+                                </span>
                                 <input
                                     type="text"
                                     name="x"
@@ -367,7 +377,12 @@ if (!is_company_manager(get_current_user_id(), $company_id)  && $company_id == 0
                         <div class="input__wrapper">
                             <label for="instagram">Instagram</label>
                             <div class="input-group">
-                                <span class="prefix">@</span>
+                                <span class="prefix">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+  <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+  <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+</svg>
+                                </span>
                                 <input
                                     type="text"
                                     name="instagram"
@@ -385,7 +400,12 @@ if (!is_company_manager(get_current_user_id(), $company_id)  && $company_id == 0
                         <div class="input__wrapper">
                             <label for="bluesky">Bluesky</label>
                             <div class="input-group">
-                                <span class="prefix">@</span>
+                                <span class="prefix">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+  <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z"/>
+  <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z"/>
+</svg>
+                                </span>
                                 <input
                                     type="text"
                                     name="bluesky"
