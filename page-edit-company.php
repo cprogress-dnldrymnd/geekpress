3876f119573f7fcb4f06dea9846101a9d6476c4d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-11-13 16:21:29 +0800

    Update page-edit-company.php

--- page-edit-company.php
+++ page-edit-company.php
@@ -68,10 +68,9 @@ get_header();
 </style>
 <?php
 $user_id = get_current_user_id();
-$company_id = $_GET['id'];
-echo is_company_manager(get_current_user_id(), $company_id) ;
+$company_id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
-if (is_company_manager(get_current_user_id(), $company_id)  && isset($_GET['id'])) {
+if (is_company_manager(get_current_user_id(), $company_id)  && $company_id != 0) {
 ?>
     <section class="edit__profile">
         <div class="container">
